75765078c16346941c6a1e151eed7707e20ffb47
HiepTrinh <xmikecolx@gmail.com>
2022-08-30 19:04:32 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// Grabbing each element by their unique id's
+const inputEl = document.getElementById('input');
+const buttonEl = document.getElementById('submit-button');
+const ulEl = document.getElementById('todo-list');
+const resetButton = document.getElementById('reset');
+
+// Adding event listeners to interact with them
+buttonEl.addEventListener('click', noteCreator);
+
+ulEl.addEventListener('click', removeNote)
+
+resetButton.addEventListener('click', clearAll)
+
+function clearAll(e) {
+  const liElements = document.querySelectorAll('li');
+  liElements.forEach(element => {
+    element.innerText = '';
+  })
+}
+
+function removeNote(e) {
+  return e.target.remove();
+}
+
+
+
+function noteCreator(e) {
+  const liElement = document.createElement('li');
+  liElement.textContent = inputEl.value;
+  inputEl.value = '';
+  ulEl.appendChild(liElement);
+}
+
+
+
+
